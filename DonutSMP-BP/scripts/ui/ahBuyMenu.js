a628305a9313f5e82255d2e5bb59d97644bf89a0
Arvie1290 <arviefaezaalauya3@gmail.com>
2026-05-31 22:15:08 +0700

    Update ahBuyMenu.js

--- DonutSMP-BP/scripts/ui/ahBuyMenu.js
+++ DonutSMP-BP/scripts/ui/ahBuyMenu.js
@@ -1,7 +1,151 @@
-// Auction House Buy Menu
-import { auctionManager } from '../auction/auctionManager';
+import {
+    MessageFormData
+} from "@minecraft/server-ui";
 
-export function ahBuyMenu(player) {
-  player.sendMessage('§l§6=== Buy Items ==="');
-  // Display listings from auction manager
+import {
+    deserializeItem
+} from "../utils/itemSerializer.js";
+
+import {
+    hasMoney,
+    removeMoney
+} from "../economy/moneyManager.js";
+
+import {
+    addClaimMoney
+} from "../database/playerDatabase.js";
+
+import {
+    deleteListing
+} from "../auction/listingManager.js";
+
+import {
+    formatPrice
+} from "../utils/priceParser.js";
+
+import {
+    formatRemainingTime
+} from "../utils/timeUtils.js";
+
+import {
+    openAHMenu
+} from "./ahMainMenu.js";
+
+export async function openBuyMenu(
+    player,
+    listing
+) {
+    const form =
+        new MessageFormData();
+
+    form.title(
+        "Confirm Purchase"
+    );
+
+    form.body(
+`Seller: ${listing.seller}
+
+Bought money: ${formatPrice(
+listing.price
+)}
+
+Expired: ${formatRemainingTime(
+listing.expiredAt
+)}`
+    );
+
+    form.button1(
+        "§aBuy"
+    );
+
+    form.button2(
+        "§cCancel"
+    );
+
+    const result =
+        await form.show(
+            player
+        );
+
+    if (
+        result.canceled
+    ) {
+        return;
+    }
+
+    /*
+    button1 = Buy
+    button2 = Cancel
+
+    selection:
+    0 = button1
+    1 = button2
+    */
+
+    if (
+        result.selection === 1
+    ) {
+        openAHMenu(
+            player
+        );
+
+        return;
+    }
+
+    if (
+        !hasMoney(
+            player,
+            listing.price
+        )
+    ) {
+        player.sendMessage(
+            "§cNot enough money."
+        );
+
+        return;
+    }
+
+    const inventory =
+        player.getComponent(
+            "minecraft:inventory"
+        );
+
+    const container =
+        inventory.container;
+
+    const item =
+        deserializeItem(
+            listing.item
+        );
+
+    const remain =
+        container.addItem(
+            item
+        );
+
+    if (remain) {
+        player.sendMessage(
+            "§cInventory full."
+        );
+
+        return;
+    }
+
+    removeMoney(
+        player,
+        listing.price
+    );
+
+    addClaimMoney(
+        listing.seller,
+        listing.price
+    );
+
+    deleteListing(
+        listing.id
+    );
+
+    player.sendMessage(
+        `§i[§1AH§i]: Successful Bought ${item.typeId} x${item.amount}!`
+    );
 }

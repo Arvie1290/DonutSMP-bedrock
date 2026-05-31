38982ccc57f0f50c57c1f6f1bf99ee9e8d076282
Arvie1290 <arviefaezaalauya3@gmail.com>
2026-05-31 22:13:18 +0700

    Update playerDatabase.js

--- DonutSMP-BP/scripts/database/playerDatabase.js
+++ DonutSMP-BP/scripts/database/playerDatabase.js
@@ -1,22 +1,61 @@
-// Player Database
-export class PlayerDatabase {
-  constructor() {
-    this.players = {};
-  }
-  
-  getPlayer(playerId) {
-    return this.players[playerId];
-  }
-  
-  savePlayer(playerId, data) {
-    this.players[playerId] = data;
-  }
-  
-  updatePlayer(playerId, data) {
-    if (this.players[playerId]) {
-      this.players[playerId] = { ...this.players[playerId], ...data };
-    }
-  }
+import { world } from "@minecraft/server";
+
+const PREFIX =
+    "donutsmp_player_";
+
+function getKey(
+    playerName
+) {
+    return (
+        PREFIX +
+        playerName
+    );
 }
 
-export const playerDatabase = new PlayerDatabase();
+function getDefaultData() {
+    return {
+        expiredItems: [],
+
+        claimMoney: [],
+
+        history: []
+    };
+}
+
+export function getPlayerData(
+    playerName
+) {
+    try {
+        const raw =
+            world.getDynamicProperty(
+                getKey(
+                    playerName
+                )
+            );
+
+        if (!raw) {
+            return getDefaultData();
+        }
+
+        return JSON.parse(
+            raw
+        );
+    }
+    catch {
+        return getDefaultData();
+    }
+}
+
+export function savePlayerData(
+    playerName,
+    data
+) {
+    world.setDynamicProperty(
+        getKey(
+            playerName
+        ),
+        JSON.stringify(
+            data
+        )
+    );
+}

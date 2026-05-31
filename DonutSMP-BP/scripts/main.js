5fa4a102ee842b201b520d3a2518fe7036c48113
Arvie1290 <arviefaezaalauya3@gmail.com>
2026-05-31 22:40:02 +0700

    Update main.js

--- DonutSMP-BP/scripts/main.js
+++ DonutSMP-BP/scripts/main.js
@@ -1,11 +1,4 @@
-/**
- * DonutSMP
- * Main Entry
- */
-
 import "./commands/ahCommand.js";
 import "./commands/sellCommand.js";
 
 import "./auction/expireManager.js";
-
-console.warn("[DonutSMP] Loaded successfully.");
